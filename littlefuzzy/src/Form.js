368515d2e65809b1e185297a1ec0cee1b8709bc1
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-30 18:28:00 -0500

    added a form test, validation, and onChange, function, forgot to commit after each function completion

--- littlefuzzy/src/Form.js
+++ littlefuzzy/src/Form.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import * as yup from 'yup';
-
+import formTest from './Validation';
 
 
 function Form (props) {
@@ -9,19 +9,59 @@ function Form (props) {
     const initialFormValues = {
         name: '',
         email: '',
-        password: '',
+        password: ''
+    
+    }
+
+    // Making Initial form errors
+    const initialFormError = {
+        name: '',
+        email: '',
+        password: ''
     
     }
 
     // Storing InitialValues inside of useState
     const [formValues, setFormValues] = useState(initialFormValues)
+    const [formError, setFormError] = useState(initialFormError)
+
+    // Making an onChange function for onChange
+    const onChange = (event) => {
+
+        // Defining Name, type, checked, value = to the event 
+        const {name, type, checked, value} = event.targe.value
+
+        // Making variable to grab the correct value of the checked boxes
+        const checkerValue = type === 'checkbox' ? checked : value 
+    }
+
+    // Making a validator for boolean values
+    function validator (name, value) {
+        yup.reach(formTest, name)
+        .validate(value)
+        .then(() => setFormError({...formError, [name]: ''}))
+        .catch(err => setFormError({...formError, [err]: err.formError[0]}))
+    }
+
+    // Making a Change function for onChange
+    function change(name, value) {
+
+        // Passing the name of the input Name and the Value
+        validator(name,value)
+
+        // Changing form values
+        setFormValues({...formValues, [name]: value})
+    }
 
     return (
-        <div>
-            <form>
-
-            </form>
-        </div>
+        <form>
+            <div className='errors'>
+                {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+                <div>{formError.name}</div>
+                <div>{formError.email}</div>
+                <div>{formError.password}</div>
+            </div>
+        </form>
     )
 }
 
--- littlefuzzy/src/Validation.js
+++ littlefuzzy/src/Validation.js
@@ -1,5 +1,21 @@
 import * as yup from 'yup';
 
+// Making Form Value Test for Form component
+
 const formTest = yup.object().shape({
-    
-})
+    name: yup
+    .string()
+    .trim()
+    .required('Name is required'),
+
+    email: yup
+    .string()
+    .email()
+    .required('Email is required'),
+
+    password: yup
+    .string()
+    .required('Password is required')
+}) 
+
+export default formTest
